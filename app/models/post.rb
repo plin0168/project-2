798f694d00602a4f02ca50dc8ab925ce569b5718
Philip Lin <philipo227@hotmail.com>
2016-09-20 16:25:02 -0700

    creating posts to user relationship

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,8 @@ class Post < ActiveRecord::Base
 
   has_attached_file :image, styles: { :medium => "640x" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
+  belongs_to :user
 end
 
 # add has_attached_file to post model after implementing paperclip gem
