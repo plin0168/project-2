3f988b6c5887f3a752dc0fdde154ebead245e2ab
Philip Lin <philipo227@hotmail.com>
2016-09-20 16:20:01 -0700

    add authenticate_user! to posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-
+  before_action :authenticate_user!
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   #collects all the posts in your Post model
   def index
