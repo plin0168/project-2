911099c371514c8b3c7aec0d18c280d585935eb8
Philip Lin <philipo227@hotmail.com>
2016-09-20 19:47:17 -0700

    update route file to include new routes for the sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   #with function index view set up, set index action
   root 'posts#index'
 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
   # The priority is based upon order of creation: first created -> highest priority.
