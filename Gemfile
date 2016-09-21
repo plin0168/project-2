f76d0f8fc8012f700e15e11a4926b1d7c7f91241
Philip Lin <philipo227@hotmail.com>
2016-09-20 19:26:00 -0700

    generate model user and bycrpt gem file, uninstall devise gem

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source 'https://rubygems.org'
 
+gem 'bcrypt', '~> 3.1.7'
+
 gem 'bootstrap-sass', '~> 3.3.5'
 
 gem 'simple_form'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160920002045) do
+ActiveRecord::Schema.define(version: 20160921022417) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "caption"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 20160920002045) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
